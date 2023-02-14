3aa905c7cef37c83d7e6f8d791cbdb04b13eba3a
dmitryukvyacheslav <dmitryuk.slavik@yandex.ru>
2023-02-14 13:47:43 +0700

    initial commit, sync cmgpu and awp

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct Point{
+	float x;
+	float y;
+}
+
+struct Shape{
+	char* name;
+	int ptscnt;
+	struct Point pts[];
+	int radius;
+}
+
+int getNextToken(int token_type){
+	
+}
+
+struct Shape parseInput{
+	
+}
+int main(void){
+	char* input = "circle(0 0, 1.5)";
+}
