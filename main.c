7da9233219809df50f53c3d9f952b4e896b7c95e
Дмитрюк Вячеслав Вячеславович <is241s06@awp01.csc.local>
2023-02-14 14:25:51 +0700

    sync awp01->cmgpu

--- main.c
+++ main.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
 
+#define TT_STRING 	0
+#define TT_POINT 	1
+#define TT_INT 		2
+
+// todo: enum argtypes and shape types
 struct Point{
 	float x;
 	float y;
 }
 
 struct Shape{
-	char* name;
+	int type;
 	int ptscnt;
 	struct Point pts[];
 	int radius;
 }
 
-int getNextToken(int token_type){
+int getShapeType(char* string, int startColumn){
+	// skip all spaces prior to first character
+	int i = startColumn;
+	while((string[i] == ' ') and (string[i] != '\0')) i++;
 	
+	// do we even have symbols in given input?
+	if (string[i] == '\0') {
+		throwError(startColumn, "'circle', 'triangle' or 'polygon'");
+		return NULL;
+	} 
+	
+	// get new lowercase string char by char until met by ( or ' '
+	char* buf[16];
+	int j = 0;
+	while((string[i] != ' ') and (string[i] != '\0') and (j < 16)) {
+		buf[j] = tolower(string[i]);
+		i++;
+		j++;
+	}
+	if (!strcmp(str, "circle")
 }
 
-struct Shape parseInput{
-	
-}
+
 int main(void){
 	char* input = "circle(0 0, 1.5)";
 }
