76632efa1e043c150b70742813b8aea008e5cb81
Дмитрюк Вячеслав Вячеславович <is241s06@awp01.csc.local>
2023-02-21 14:10:19 +0700

    Implement input from stdin

--- main.c
+++ main.c
@@ -1,42 +1,26 @@
 #include "shapes.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void){
-	shape* s1 = NULL;
-	char* input = "     circle(    1.1  2 , 1.24   )    ";
-	s1 = parseInputString(input, 0);
-	
-
 	printf("1. ");
-	switch (s1->type)
-	{
-	case Circle:
-		printf("circle(");
-		break;
+	char *buffer = NULL;
+    //int read;
+    size_t len;
+    getline(&buffer, &len, stdin);
+    //if (-1 != read) puts(buffer);
 	
-	case Triangle:
-		printf("triangle(");
-		break;
-
-	case Polygon:
-		printf("polygon(");
-		break;
-	}
-	for (int i = 0; i < s1->ptscnt; i++) {
-		printf("%.2f %.2f", s1->pts[i].x, s1->pts[i].y);
-		if (i != s1->ptscnt-1 || s1->type == Circle) printf(", ");  
-	}
-	
+	shape* s1 = NULL;
+	s1 = parseInputString(buffer, 0);
+	if (s1 == NULL) return -1;
 
 	switch (s1->type)
 	{
 	case Circle:
-		printf("%.2f)\n", s1->radius);
 		printf("\tperimeter = %.4f\n", 2.0*3.14*s1->radius);
 		printf("\tarea = %.4f\n", 3.14*s1->radius*s1->radius);
 		break;
 	default:
-		printf(")\n");
 		printf("not implemented\n");
 	}
 	return 0;
