673fde3a2035a29947327a7ceeaff6950cc7471f
Дмитрюк Вячеслав Вячеславович <is241s06@awp01.csc.local>
2023-02-21 14:25:03 +0700

    Add math_h M_PI, add proper formatting

--- main.c
+++ main.c
@@ -1,27 +1,27 @@
 #include "shapes.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main(void){
-	printf("1. ");
-	char *buffer = NULL;
-    //int read;
+int main(void)
+{
+    printf("1. ");
+    char* buffer = NULL;
     size_t len;
     getline(&buffer, &len, stdin);
-    //if (-1 != read) puts(buffer);
-	
-	shape* s1 = NULL;
-	s1 = parseInputString(buffer, 0);
-	if (s1 == NULL) return -1;
 
-	switch (s1->type)
-	{
-	case Circle:
-		printf("\tperimeter = %.4f\n", 2.0*3.14*s1->radius);
-		printf("\tarea = %.4f\n", 3.14*s1->radius*s1->radius);
-		break;
-	default:
-		printf("not implemented\n");
-	}
-	return 0;
+    shape* s1 = NULL;
+    s1 = parseInputString(buffer, 0);
+    if (s1 == NULL)
+        return -1;
+
+    switch (s1->type) {
+    case Circle:
+        printf("\tperimeter = %.4f\n", 2.0 * M_PI * s1->radius);
+        printf("\tarea = %.4f\n", M_PI * s1->radius * s1->radius);
+        break;
+    default:
+        printf("not implemented\n");
+    }
+    return 0;
 }
--- shapes.h
+++ shapes.h
@@ -1,19 +1,15 @@
-enum ShapeTypes {
-	Circle = 10,
-	Triangle = 11,
-	Polygon = 12
-};
+enum ShapeTypes { Circle = 10, Triangle = 11, Polygon = 12 };
 
-typedef struct Point{
-	float x;
-	float y;
+typedef struct Point {
+    float x;
+    float y;
 } point;
 
-typedef struct Shape{
-	enum ShapeTypes type;
-	int ptscnt;
-	float radius;
-	point* pts;
+typedef struct Shape {
+    enum ShapeTypes type;
+    int ptscnt;
+    float radius;
+    point* pts;
 } shape;
 
 shape* parseInputString(char* input, int startSymbol);
