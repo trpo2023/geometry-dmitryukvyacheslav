5e477d783248045908a59c5b8005efb7684dcde5
dmitryukvyacheslav <dmitryuk.slavik@yandex.ru>
2023-04-29 16:18:21 +0700

    format

--- src/test/tests.c
+++ src/test/tests.c
@@ -1,17 +1,19 @@
-#include <libgeometry/parser.h>
 #include <libgeometry/intersect.h>
 #include <libgeometry/lexer.h>
+#include <libgeometry/parser.h>
 #include <stdio.h>
 #include <thirdparty/ctest/ctest.h>
 
-CTEST(parsing, lex_get_type) {
+CTEST(parsing, lex_get_type)
+{
     shape shapes[5];
-    char* strings[5] =  {"   CirCle(","circle    (","circl    (", "circle", " "};
+    char* strings[5]
+            = {"   CirCle(", "circle    (", "circl    (", "circle", " "};
     for (int i = 0; i < 4; i++)
         lex_get_type(&(strings[i]), &(shapes[i]));
     ASSERT_EQUAL(shapes[0].type, Circle);
     ASSERT_EQUAL(shapes[1].type, Circle);
     ASSERT_NOT_EQUAL(shapes[2].type, Circle);
-    ASSERT_NOT_EQUAL(shapes[3].type, Circle);    
-    ASSERT_NOT_EQUAL(shapes[4].type, Circle);   
+    ASSERT_NOT_EQUAL(shapes[3].type, Circle);
+    ASSERT_NOT_EQUAL(shapes[4].type, Circle);
 }
